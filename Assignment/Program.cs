71a375ae2a6a36ca172d68b381896e3e295e3e9f
D-Elbadawi <130947785+D-Elbadawi@users.noreply.github.com>
2023-06-01 17:36:28 +0000

    Code changed

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,10 +6,6 @@ namespace Assignment
     {
         static void Main()
         {
-            // You could try creating a class that takes user input to produce packs
-            // if you want more practice (PackMerchant or something)
-            // You can comment out this code until you are ready to use it if you 
-            // want to use the Main method for something else.
             const int PackMaxItems = 10;
             const float PackMaxVolume = 20;
             const float PackMaxWeight = 30;
